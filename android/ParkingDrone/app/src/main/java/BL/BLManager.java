66e90e722d6c30349e1bf40a4990a6263afedfb7
barrhaim <barrhaim@gmail.com>
2018-10-21 12:05:08 +0300

    files

--- android/ParkingDrone/app/src/main/java/BL/BLManager.java
+++ android/ParkingDrone/app/src/main/java/BL/BLManager.java
@@ -1,18 +1,52 @@
 package BL;
 
+import android.arch.core.util.Function;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.base.BaseComponent;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.sdkmanager.DJISDKManager;
+
 public class BLManager {
     private static BLManager instance = null;
     DroneSingleton  drone;
     SocketManager socket_manager;
 
-    private BLManager(){
+
+    private BLManager()
+    {
         drone = DroneSingleton.getInstance();
         socket_manager = SocketManager.getInstance();
     }
+
+    public static CompletableFuture<Boolean> init(Context context)
+    {
+        return null;
+    }
     public static BLManager getInstance(){
-        if(instance == null){
-            instance = new BLManager();
-        }
         return instance;
     }
+
+
+    private class Register extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return "Download failed";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+}
 }
--- android/ParkingDrone/app/src/main/java/BL/DroneSingleton.java
+++ android/ParkingDrone/app/src/main/java/BL/DroneSingleton.java
@@ -11,4 +11,5 @@ public class DroneSingleton {
             drone = new DroneSingleton();
         return drone;
     }
+
 }
