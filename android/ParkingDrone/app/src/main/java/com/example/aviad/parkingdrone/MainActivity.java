d391508aa65d1d2aa85a38c36f8c7c410944c12f
aviad31 <mikud40@gmail.com>
2018-11-02 17:00:29 +0200

    delete zahi log and firebase

--- android/ParkingDrone/app/src/main/java/com/example/aviad/parkingdrone/MainActivity.java
+++ android/ParkingDrone/app/src/main/java/com/example/aviad/parkingdrone/MainActivity.java
@@ -3,14 +3,9 @@ package com.example.aviad.parkingdrone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 import BL.BLManager;
-import SharedClasses.MyLogger;
-import SharedClasses.RemoteLogCat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-
-        MyLogger.getInstance().mFirebaseAnalytics=FirebaseAnalytics.getInstance(this);
-        MyLogger.getInstance().debug("test");
 
          bl_manager = BLManager.getInstance();
 
