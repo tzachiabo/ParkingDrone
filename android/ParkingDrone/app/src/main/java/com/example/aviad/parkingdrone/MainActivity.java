32d9350880fb183b3c83e1a893aa430a922e7df5
aviad31 <mikud40@gmail.com>
2018-11-03 12:43:08 +0200

    remove tests

--- android/ParkingDrone/app/src/main/java/com/example/aviad/parkingdrone/MainActivity.java
+++ android/ParkingDrone/app/src/main/java/com/example/aviad/parkingdrone/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for (int i=0;i<100;i++)
-            RemoteLogCat.error("try "+ i);
          bl_manager = BLManager.getInstance();
 
     }
