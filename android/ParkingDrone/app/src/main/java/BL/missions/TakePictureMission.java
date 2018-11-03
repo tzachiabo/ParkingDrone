3b0a72a0cc70d2904c005da6eddc2ada09fcdd96
aviad31 <mikud40@gmail.com>
2018-11-03 16:41:30 +0200

    TakePictureMission take pic

--- android/ParkingDrone/app/src/main/java/BL/missions/TakePictureMission.java
+++ android/ParkingDrone/app/src/main/java/BL/missions/TakePictureMission.java
@@ -1,7 +1,22 @@
 package BL.missions;
 
-public class TakePictureMission extends Mission {
+import android.os.Handler;
 
+import com.dji.mapkit.core.camera.DJICameraUpdateFactory;
+
+import SharedClasses.RemoteLogCat;
+import dji.common.camera.SettingsDefinitions;
+import dji.common.error.DJIError;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.camera.*;
+import dji.common.camera.*;
+import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+
+public class TakePictureMission extends Mission {
+    final Camera camera= DJISDKManager.getInstance().getProduct().getCamera();
+    private Handler handler;
     int size;
     byte[] picture;
 
@@ -11,6 +26,33 @@ public class TakePictureMission extends Mission {
 
     @Override
     public void start() {
+        SettingsDefinitions.ShootPhotoMode photoMode = SettingsDefinitions.ShootPhotoMode.SINGLE;
+        handler = new Handler();
+        camera.setShootPhotoMode(photoMode,new CommonCallbacks.CompletionCallback(){
+            @Override
+            public void onResult(DJIError djiError) {
+                if (null == djiError) {
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            camera.startShootPhoto(new CommonCallbacks.CompletionCallback() {
+                                @Override
+                                public void onResult(DJIError djiError) {
+                                    if (djiError == null) {
+                                        RemoteLogCat.debug("take photo: success");
+                                    } else {
+                                        RemoteLogCat.error(djiError.getDescription());
+                                    }
+                                }
+                            });
+                        }
+                    }, 2000);
+                }
+            }
+        });
+
+
+
 
     }
 
