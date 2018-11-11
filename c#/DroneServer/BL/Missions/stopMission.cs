fdba1ba0049f33eb7db8fb8b05caa455d30e911e
tzachi46 <tzachiabo@gmail.com>
2018-11-11 20:21:10 +0200

    add stop mission

---
+++ c#/DroneServer/BL/Missions/stopMission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServer.BL.Missions
+{
+    class stopMission : LeafMission
+    {
+        public override string encode()
+        {
+            return "stop " + m_index;
+        }
+
+        public override void stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
