2b332bd56493c1e9ff0a3d89eefa281d20e0c9b1
aviad31 <mikud40@gmail.com>
2018-11-13 10:18:40 +0200

    UnitTestProjectand Go to base added

--- c#/DroneServer/BL/Missions/InitParkingMission.cs
+++ c#/DroneServer/BL/Missions/InitParkingMission.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DroneServer.SharedClasses;
+
 
 namespace DroneServer.BL.Missions
 {
-    class InitParkingMission : ComplexMission
+    public class InitParkingMission : ComplexMission
     {
-        public override void execute()
+        private GPSPoint gps1;
+        private GPSPoint gps2;
+        private GPSPoint gps3;
+        private GPSPoint gps4;
+        
+        public InitParkingMission(GPSPoint gps1, GPSPoint gps2, GPSPoint gps3, GPSPoint gps4) : this(null, gps1, gps2, gps3, gps4)
         {
 
+        }
+        public InitParkingMission(ComplexMission parent_mission, GPSPoint gps1, GPSPoint gps2, GPSPoint gps3, GPSPoint gps4) : base()
+        {
+            m_ParentMission = parent_mission;
+            GPSPoint basePoint = getBaseLocation(gps1, gps2, gps3, gps4);
+            m_SubMission.Enqueue(new MoveToGPSPoint(basePoint.x, basePoint.y, basePoint.z));
+            
+        }
+        public override void execute()
+        {
+            Mission mission = m_SubMission.Dequeue();
+
+            mission.execute();
         }
         public override void done()
         {
@@ -18,6 +38,24 @@ namespace DroneServer.BL.Missions
         }
         public override void stop()
         {
+
+        }
+        public static GPSPoint getBaseLocation(GPSPoint gps1, GPSPoint gps2, GPSPoint gps3, GPSPoint gps4)
+        {
+            var Xlist = new[] { gps1.x, gps2.x, gps3.x, gps4.x };
+            var Ylist = new[] { gps1.y, gps2.y, gps3.y, gps4.y };
+            var Zlist = new[] { gps1.z, gps2.z, gps3.z, gps4.z };
+            double minX = Xlist.Min();
+            double maxX = Xlist.Max();
+            double minY = Ylist.Min();
+            double maxY = Ylist.Max();
+            double maxZ = Zlist.Max();
+
+            double middleX = (minX + maxX) / 2;
+            double middleY = (minY + maxY) / 2;
+            Configuration conf = Configuration.getInstance();
+            double highet = Math.Sqrt(Math.Pow((maxX - minX), 2) + Math.Pow((maxY - minY), 2)) / conf.getHighetfix()+ maxZ;
+            return new GPSPoint(middleX, middleY, highet);
 
         }
 
---
+++ c#/UnitTestProject/initParkingMissionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DroneServer.SharedClasses;
+using DroneServer.BL;
+using DroneServer.BL.Missions;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class initParkingMissionTest
+    {
+        [TestMethod]
+        public void getBaseLocationWith4GPSPoints()
+        {
+            GPSPoint point1 = new GPSPoint(0,0,0);
+            GPSPoint point2 = new GPSPoint(0, 3, 0);
+            GPSPoint point3 = new GPSPoint(4, 0, 0);
+            GPSPoint point4 = new GPSPoint(4, 3, 0);
+            GPSPoint basePoint = InitParkingMission.getBaseLocation(point1, point2, point3, point4);
+            Assert.IsTrue(basePoint.x == 2);
+            Assert.IsTrue(basePoint.y == 1.5);
+            Assert.IsTrue(basePoint.z == 5/2.144);
+        }
+    }
+}
