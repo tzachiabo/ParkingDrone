69de738845a7d5ede1fdd75d2724ba262e9b7f47
tzachi46 <tzachiabo@gmail.com>
2018-11-18 14:35:06 +0200

    add getDroneStatus in server

---
+++ c#/DroneServer/BL/Missions/LeafMission/getDroneStatus.cs
@@ -0,0 +1,31 @@
+﻿using DroneServer.SharedClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServer.BL.Missions
+{
+    class getDroneStatus : LeafMission
+    {
+        public getDroneStatus() { }
+
+        public getDroneStatus(ComplexMission mission) { m_ParentMission = mission; }
+        
+        public override string encode()
+        {
+            return "getStatus" + m_index;
+        }
+
+        public override void stop()
+        {
+            Assertions.verify(false, "get status -stop not implemented yet");
+        }
+
+        public override void done(Response response)
+        {
+            Assertions.verify(false, "get status -done not implemented yet");
+        }
+    }
+}
