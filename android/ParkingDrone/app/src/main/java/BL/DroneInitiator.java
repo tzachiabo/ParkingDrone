846854732239aa6f9317f1e84419cd6a54b18fe1
tzachi46 <tzachiabo@gmail.com>
2018-11-20 15:15:59 +0200

    intreduce executor to android

--- android/ParkingDrone/app/src/main/java/BL/DroneInitiator.java
+++ android/ParkingDrone/app/src/main/java/BL/DroneInitiator.java
@@ -1,91 +1,49 @@
 package BL;
 
-import SharedClasses.Config;
-import SharedClasses.DroneStatus;
-import SharedClasses.Logger;
-
-import android.os.Environment;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import BL.missions.TakePictureMission;
 import SharedClasses.Assertions;
+import SharedClasses.Config;
+import SharedClasses.DroneStatus;
+import SharedClasses.Logger;
 import dji.common.camera.SettingsDefinitions;
 import dji.common.error.DJIError;
-import dji.common.flightcontroller.LocationCoordinate3D;
 import dji.common.flightcontroller.virtualstick.FlightCoordinateSystem;
 import dji.common.flightcontroller.virtualstick.RollPitchControlMode;
 import dji.common.flightcontroller.virtualstick.VerticalControlMode;
 import dji.common.flightcontroller.virtualstick.YawControlMode;
-import dji.common.model.LocationCoordinate2D;
 import dji.common.util.CommonCallbacks;
 import dji.sdk.camera.Camera;
 import dji.sdk.products.Aircraft;
 import dji.sdk.sdkmanager.DJISDKManager;
 
-public class BLManager {
-    private static BLManager instance = null;
-    private SocketManager socket_manager;
-    public File file;
-    private AtomicInteger DroneStatus;
-    private boolean isConnected;
+public class DroneInitiator {
 
-    private BLManager() {
-        Logger.debug("initiate BL");
-        isConnected = false;
-        socket_manager = SocketManager.getInstance();
-        DroneStatus = new AtomicInteger();
+    private boolean isCameraInitiated;
+    private boolean isFlightControllerInitiated;
 
-    }
-
-    public static BLManager getInstance() {
-        if (instance == null) {
-            instance = new BLManager();
+    public static void init(){
+        DroneInitiator drone = new DroneInitiator();
+        if (!drone.isFlightControllerInitiated)
+        {
+            drone.initFlightController();
         }
-        return instance;
-    }
-
-<<<<<<< HEAD
-    public synchronized void init() {
-        initFs();
-        initCamera();
-        initFlightController();
-        initHomeLocation();
-=======
-    public synchronized void init(){
-        if(!isDroneReady()) {
-            initFs();
+        if (!drone.isCameraInitiated)
+        {
+            drone.initCamera();
         }
->>>>>>> c5ee8fd... tmp
-        isConnected = true;
     }
 
-    public synchronized void DisconnectDrone() {
-        isConnected = false;
+    private DroneInitiator(){
+        isCameraInitiated = false;
+        isFlightControllerInitiated = false;
     }
 
-    public synchronized SharedClasses.DroneStatus getDroneStatus() {
-        if (isDroneReady() && isConnected)
-            return SharedClasses.DroneStatus.Connected;
-        else if (isConnected)
-            return SharedClasses.DroneStatus.NotReady;
-        else
-            return SharedClasses.DroneStatus.Disconnected;
-    }
-
-<<<<<<< HEAD
-    private void initFlightController() {
+    private void initFlightController(){
         Aircraft aircraft = (Aircraft) DJISDKManager.getInstance().getProduct();
         Assertions.verify(aircraft != null, "while init flight controller aircraft was null");
+
         aircraft.getFlightController().setRollPitchControlMode(RollPitchControlMode.VELOCITY);
         aircraft.getFlightController().setYawControlMode(YawControlMode.ANGULAR_VELOCITY);
         aircraft.getFlightController().setVerticalControlMode(VerticalControlMode.VELOCITY);
@@ -99,27 +57,12 @@ public class BLManager {
                     Assertions.verify(false, "failed to set virtual stick");
                 } else {
                     Logger.info("init FlightController has been done");
-                    DroneStatus.getAndIncrement();
+                    isFlightControllerInitiated = true;
                 }
             }
         });
     }
-=======
->>>>>>> 7a6450d... intreduce executor to android
 
-    public boolean isDroneReady() {
-        return DroneStatus.get() == 4;
-    }
-
-    private void initFs() {
-        file = new File(Environment.getExternalStorageDirectory(),
-                Config.DJI_PHOTO_DIR);
-        file.mkdirs();
-        Logger.info("main dir" + file.getAbsolutePath());
-        DroneStatus.getAndIncrement();
-    }
-
-<<<<<<< HEAD
     private void initCamera() {
         Camera camera = getCamera();
         TakePictureMission.camera = camera;
@@ -131,13 +74,13 @@ public class BLManager {
                     Assertions.verify(false, "failed to set camera mode to single");
                 } else {
                     Logger.info("Photo mode is Single");
-                    DroneStatus.getAndIncrement();
+                    isCameraInitiated = true;
                 }
             }
         });
     }
 
-    private Camera getCamera() {
+    private Camera getCamera(){
         Aircraft aircraft = (Aircraft) DJISDKManager.getInstance().getProduct();
         Assertions.verify(aircraft != null, "while init camera aircraft was null");
         List<Camera> cameras = DJISDKManager.getInstance().getProduct().getCameras();
@@ -148,12 +91,8 @@ public class BLManager {
             }
         }
 
-        Assertions.verify(false, "camera " + Config.MAIN_CAMERA_NAME + "could not be found");
+        Assertions.verify(false, "camera "+ Config.MAIN_CAMERA_NAME +"could not be found");
         return null;
     }
-=======
->>>>>>> 7a6450d... intreduce executor to android
-
-
 
 }
