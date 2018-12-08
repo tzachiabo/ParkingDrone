08406014df8faf9d0b39a7bd0d7c8ff4ff4baa71
barrhaim <barrhaim@gmail.com>
2018-12-08 19:20:54 +0200

    rdy for testing

--- android/ParkingDrone/app/src/main/java/BL/Drone/DJIM210/MissionControlManager.java
+++ android/ParkingDrone/app/src/main/java/BL/Drone/DJIM210/MissionControlManager.java
@@ -175,12 +175,36 @@ public class MissionControlManager {
         return genPoints(mixedpoints);
     }
 
+    private List<LocationCoordinate3D> genZPoints(List<LocationCoordinate3D> points) {
+        List<LocationCoordinate3D> newpoints = new ArrayList<>();
+        List<LocationCoordinate3D> mixedpoints = new ArrayList<>();
+        if (Math.abs(points.get(0).getAltitude() - points.get(1).getAltitude()) < 10) {
+            return points;
+        }
+        for (int i = 0; i < points.size() - 1; i++) {
+            float newz = (points.get(0).getAltitude() + points.get(1).getAltitude()) / 2;
+            newpoints.add(new LocationCoordinate3D(points.get(0).getLatitude(),points.get(0).getLongitude()
+                    , newz));
+        }
+        int times_to_run = newpoints.size() + points.size();
+        for (int i = 0; i < times_to_run ; i++) {
+            if (i % 2 == 0) {
+                mixedpoints.add(points.get(0));
+                points.remove(0);
+            } else {
+                mixedpoints.add(newpoints.get(0));
+                newpoints.remove(0);
+            }
+        }
+        return genPoints(mixedpoints);
+    }
+
     private WaypointMission.Builder buildWithSubWaypoints(double x, double y, float z, WaypointMission.Builder wpm) {
         LocationCoordinate3D currentloc = M210Manager.getInstance().getDroneStatus();
         List<LocationCoordinate3D> points = new ArrayList<>();
         points.add(currentloc);
         points.add(new LocationCoordinate3D(x, y, z));
-        List<LocationCoordinate3D> locationCoordinate3DS = genPoints(points);
+        List<LocationCoordinate3D> locationCoordinate3DS = genPoints(genZPoints(points));
         if (locationCoordinate3DS.size() == 2) {
             wpm.addWaypoint(new Waypoint(x, y + 0.00005, z));
             wpm.addWaypoint(new Waypoint(x, y, z));
@@ -192,41 +216,4 @@ public class MissionControlManager {
 
         return wpm;
     }
-//    private WaypointMission.Builder zAxisHelper(float z , WaypointMission.Builder wpm){
-//        float init_z = M210Manager.getInstance().getDroneStatus().getAltitude();
-//        double init_x = M210Manager.getInstance().getDroneStatus().getLatitude();
-//        double init_y = M210Manager.getInstance().getDroneStatus().getLongitude();
-//        float reach_to_z = init_z + 10;
-//        while(reach_to_z < z){
-//            Logger.info("Adding new WayPoint x: " + init_x + " y :" + init_y + " z :" + reach_to_z);
-//            wpm.addWaypoint(new Waypoint(init_x,init_y,reach_to_z));
-//            reach_to_z = reach_to_z + 10;
-//        }
-//        Logger.info("Adding Last Z WayPoint x: " + init_x + " y :" + init_y+ " z :" + init_z);
-//        return wpm.addWaypoint(new Waypoint(init_x,init_y,z));
-//    }
-//    private WaypointMission.Builder xyAxisHelperXlowerYlower(double x, double y, float z , WaypointMission.Builder wpm){
-//        double init_x = M210Manager.getInstance().getDroneStatus().getLatitude();
-//        double init_y = M210Manager.getInstance().getDroneStatus().getLongitude();
-//        //new_latitude  = latitude  + (dy / r_earth) * (180 / pi);
-//        //new_longitude = longitude + (dx / r_earth) * (180 / pi) / cos(latitude * pi/180);
-//        double reach_to_x = init_x + (-10/6371000)*(180*Math.PI);
-//        double reach_to_y = init_y + (-10/6371000)*(180*Math.PI) / Math.cos(init_x * (Math.PI/180)) ;
-//        Logger.debug("reach to x = " + reach_to_x + " yet x is " + x);
-//        Logger.debug("reach to y = " + reach_to_y + " yet y is " + y);
-//        while(reach_to_x < x && reach_to_y < y ){
-//            Logger.info("Adding new WayPoint x: " + reach_to_x + " y :" + reach_to_y + " z :" + z);
-//            wpm.addWaypoint(new Waypoint(reach_to_x,reach_to_y,z));
-//            if(reach_to_x < x){
-//                reach_to_x = reach_to_x + (-10/6371000)*(180*Math.PI);
-//            }
-//            if(reach_to_y < y){
-//                reach_to_y  = reach_to_y + (-10/6371000)*(180*Math.PI) / Math.cos(reach_to_x * (Math.PI/180)) ;
-//            }
-//        }
-//        Logger.info("Adding Last XY WayPoint x: " + x + " y :" + y + " z :" + z);
-//        wpm.addWaypoint(new Waypoint(x, y + 0.00005, z));
-//        return wpm.addWaypoint(new Waypoint(x,y,z));
-
-//    }
 }
