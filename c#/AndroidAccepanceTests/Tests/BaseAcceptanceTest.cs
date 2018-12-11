194bbc9b992d516dfd4d19967a3901fd1ddde79b
tzachi46 <tzachiabo@gmail.com>
2018-12-11 21:23:25 +0200

    base acceptance test

---
+++ c#/AndroidAccepanceTests/Tests/BaseAcceptanceTest.cs
@@ -0,0 +1,58 @@
+﻿using DroneServer.BL.Missions;
+using DroneServer.SharedClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidAccepanceTests
+{
+    public class BaseAcceptanceTest
+    {
+        private Comm comm = Comm.getInstance();
+
+        protected void take_off()
+        {
+            TakeOff take_off = new TakeOff();
+            CompletionHanlder take_off_mission = comm.sendMission(take_off);
+        }
+
+        protected void landing()
+        {
+            startLanding();
+            confirmLanding();
+        }
+
+        protected void startLanding()
+        {
+            DroneServer.BL.Missions.StartLanding start_landing = new DroneServer.BL.Missions.StartLanding();
+            CompletionHanlder start_landing_mission = comm.sendMission(start_landing);
+        }
+
+        protected void confirmLanding()
+        {
+            ConfirmLanding conf_landing = new ConfirmLanding();
+            CompletionHanlder conf_landing_mission = comm.sendMission(conf_landing);
+        }
+
+        protected void move(Direction direction, double distance)
+        {
+            MoveMission move = new MoveMission(direction, distance);
+            CompletionHanlder move_mission = comm.sendMission(move);
+        }
+
+        protected void goToGPS(Direction direction, double distance)
+        {
+            MoveMission move = new MoveMission(direction, distance);
+            CompletionHanlder move_mission = comm.sendMission(move);
+        }
+
+        protected Point getLocation()
+        {
+            GetLocation get_loation = new GetLocation();
+            CompletionHanlder get_loation_mission = comm.sendMission(get_loation);
+            return (Point)get_loation_mission.response.Data;
+        }
+    }
+}
---
+++ c#/DroneServer/BL/Missions/LeafMission/MoveMission.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DroneServer.SharedClasses;
+
+namespace DroneServer.BL.Missions
+{
+    public class MoveMission : LeafMission
+    {
+        private double distance;
+        private Direction direction;
+
+        public MoveMission(Direction direction, double distance) : this(null, direction, distance)
+        {
+        }
+
+        public MoveMission(ComplexMission ParentMission, Direction direction, double distance):base(ParentMission)
+        {
+            this.direction = direction;
+            this.distance = distance;
+        }
+
+        public override void stop()
+        {
+
+        }
+
+        public override string encode()
+        {
+            return "move " + m_index + " " + direction + " " + distance;
+        }
+    }
+}
