e30317261520bdcff1d42da27799ede7611286b0
barrhaim <barrhaim@gmail.com>
2018-12-11 23:06:26 +0200

    move to gps tests

--- c#/AndroidAccepanceTests/Tests/MoveToGPSTest.cs
+++ c#/AndroidAccepanceTests/Tests/MoveToGPSTest.cs
@@ -19,5 +19,43 @@ namespace AndroidAccepanceTests
 
             landing();
         }
+
+        [TestMethod]
+        public void moveToLimit()
+        {
+            take_off();
+            Point loc = getLocation();
+            move(DroneServer.SharedClasses.Direction.right, 30);
+            move(DroneServer.SharedClasses.Direction.up, 95);
+            MoveByGPS(loc.lat, loc.lng, 10);
+            loc = getLocation();
+            landing();
+        }
+
+        [TestMethod]
+        public void moveToShortLocation()
+        {
+            take_off();
+            Point loc = getLocation();
+            move(DroneServer.SharedClasses.Direction.backward, 2);
+            move(DroneServer.SharedClasses.Direction.up, 6);
+            MoveByGPS(loc.lat, loc.lng, 10);
+            loc = getLocation();
+            landing();
+        }
+
+        [TestMethod]
+        public void moveAfterMoveToGps()
+        {
+            take_off();
+            Point loc = getLocation();
+            move(DroneServer.SharedClasses.Direction.backward, 2);
+            move(DroneServer.SharedClasses.Direction.up, 6);
+            MoveByGPS(loc.lat, loc.lng, 10);
+            move(DroneServer.SharedClasses.Direction.right, 15);
+            loc = getLocation();
+            landing();
+        }
+
     }
 }
