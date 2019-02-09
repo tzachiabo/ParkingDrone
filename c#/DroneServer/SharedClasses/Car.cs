4a33d1cce7fcf49459e4003ecb7039c2106ed04c
tzachi46 <tzachiabo@gmail.com>
2019-02-09 15:42:24 +0200

    add car_detector.cs

---
+++ c#/DroneServer/SharedClasses/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServer.SharedClasses
+{
+    public class Car
+    {
+        private String type;
+        private int precent;
+        private int left_margin;
+        private int top_margin;
+        private int width;
+        private int height;
+
+        public Car(String type, int precent, int left_margin, int top_margin, int width, int height)
+        {
+            this.type = type;
+            this.precent = precent;
+            this.left_margin = left_margin;
+            this.top_margin = top_margin;
+            this.width = width;
+            this.height = height;
+        }
+
+
+    }
+}
---
+++ c#/DroneServer/car_detector.py
@@ -0,0 +1,21 @@
+#!python3
+import sys
+import os
+import subprocess
+from shutil import copyfile
+
+cwd = os.getcwd()
+base_photo_name = sys.argv[1]
+
+cwd2 = os.path.abspath(os.path.join(cwd, os.pardir))
+cwd2 = os.path.abspath(os.path.join(cwd2, os.pardir))
+cwd2 = os.path.abspath(os.path.join(cwd2, os.pardir))
+cwd2 = os.path.abspath(os.path.join(cwd2, os.pardir))
+
+cwd2 = os.path.abspath(os.path.join(cwd2, 'car_detector\\build\\darknet\\x64'))
+
+copyfile(cwd + '\\' + base_photo_name , cwd2 + '\\' + base_photo_name)
+
+
+p = subprocess.Popen(args=['darknet_no_gpu.exe','detect','yolov3-aerial.cfg','yolov3-aerial.weights','cfg/coco.data', base_photo_name], cwd=cwd2, shell=True)
+p.communicate()
