65ecb664d2ffc73029da9fbc78cbf2085eab0d0a
tzachi46 <tzachiabo@gmail.com>
2019-02-23 12:58:45 +0200

    add verify location module to mission manager

---
+++ c#/DroneServer/BL/CV/VerifyLocation.cs
@@ -0,0 +1,57 @@
+﻿using DroneServer.SharedClasses;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServer.BL.CV
+{
+    public class VerifyLocation
+    {
+        public static SharedClasses.Point getLocation(String base_photo_path, String current_photo_path, Double ratio)
+        {
+            String module_result = run_verify_location_module(base_photo_path, current_photo_path, ratio);
+
+            return parseStringToPoint(module_result);
+        }
+
+        private static String run_verify_location_module(String base_photo_path, String current_photo_path, Double ratio)
+        {
+
+            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo("python");
+
+            // make sure we can read the output from stdout 
+            myProcessStartInfo.UseShellExecute = false;
+            myProcessStartInfo.RedirectStandardOutput = true;
+            myProcessStartInfo.CreateNoWindow = true;
+
+            String myPythonApp = "verify_location.py " + base_photo_path + " " + current_photo_path + " " + ratio;
+            myProcessStartInfo.Arguments = myPythonApp;
+
+            Process myProcess = new Process();
+            myProcess.StartInfo = myProcessStartInfo;
+
+            // start the process 
+            myProcess.Start();
+            StreamReader myStreamReader = myProcess.StandardOutput;
+            string myString = myStreamReader.ReadToEnd();
+            myProcess.WaitForExit();
+
+            return myString;
+        }
+
+        private static Point parseStringToPoint(String point_str)
+        {
+            int start = point_str.IndexOf('(') + 1;
+            int end  = point_str.IndexOf(')') - 1;
+            String sub_point_str = point_str.Substring(start, end);
+
+            String[] split_point_str = sub_point_str.Split(',');
+
+            return new Point(Int32.Parse(split_point_str[0]), Int32.Parse(split_point_str[1]));
+        }
+    }
+}
---
+++ c#/UnitTestProject/BL/VerifyLocationTests/verifyLocationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DroneServer.BL.CV;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.BL
+{
+    [TestClass]
+    public class verifyLocationTest
+    {
+        [TestMethod]
+        public void verifyLocation()
+        {
+            Double ratio = 0.33;
+            VerifyLocation.getLocation("BL\\VerifyLocationTests\\big_pic.jpg", "BL\\VerifyLocationTests\\sub_big_pic.jpg", ratio);
+        }
+
+        [TestMethod]
+        public void verifyLocationSmall ()
+        {
+            Double ratio = 0.2;
+            DroneServer.SharedClasses.Point point = VerifyLocation.getLocation("BL\\VerifyLocationTests\\car.jpg", "BL\\VerifyLocationTests\\sub_car.jpg", ratio);
+            int i = 5;
+        }
+    }
+}
