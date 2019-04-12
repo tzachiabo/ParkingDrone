014e90a2e9745f6e716cf027b5a3b37ac9fc380d
aviad <mikud40@gmail.com>
2019-04-12 15:38:16 +0300

    add get car plate

--- c#/DroneServer/BL/Missions/ComplexMission/GetCarPlate.cs
+++ c#/DroneServer/BL/Missions/ComplexMission/GetCarPlate.cs
@@ -22,6 +22,7 @@ namespace DroneServer.BL.Missions
             report_mannager = ReportManager.getInstance();
             m_take_photo = take_photo.m_index;
             m_SubMission.Enqueue(rotate_mission);
+            take_photo.register_to_notification(after_take_photo);
             m_SubMission.Enqueue(take_photo);
         }
 
@@ -36,24 +37,45 @@ namespace DroneServer.BL.Missions
             return Tuple.Create(0.0, 0.0, 0.0);
         }
 
-        public override void notify(Response response)
+        private void after_take_photo(Response response)
         {
-            if (response.Key == m_take_photo)
+            String car_plate_photo_path = (String)response.Data;
+            List<string> cars = CarPlateDetector.getCarPlates(car_plate_photo_path);
+            if (cars.Count > 0)
             {
-                String car_plate_photo_path = (String)response.Data;
-                List<string> cars = CarPlateDetector.getCarPlates(car_plate_photo_path);
-                if (cars.Count > 0)
+                report_mannager.addCarPlate(cars, car_plate_photo_path);
+                done(new Response(m_index, Status.Ok, MissionType.MainMission, "succes"));
+            }
+            else
+            {
+                tries++;
+                if (tries == 1)
                 {
-                    report_mannager.addCarPlate(cars, car_plate_photo_path);
-                    done(new Response(m_index, Status.Ok, MissionType.MainMission, response.Data));
+                    MoveMission move_left = new MoveMission(Direction.left, 1.0);
+                    move_left.register_to_notification(move_finished);
+                    move_left.execute();
+                }
+                else if (tries == 2)
+                {
+                    MoveMission move_right = new MoveMission(Direction.right, 2.0);
+                    move_right.register_to_notification(move_finished);
+                    move_right.execute();
                 }
                 else
                 {
-
-                }
-
+                    done(new Response(m_index, Status.Ok, MissionType.MainMission, "fail"));
+                } 
             }
-
         }
+
+        private void move_finished(Response response)
+        {
+            TakePhoto take_pic = new TakePhoto();
+            m_take_photo = take_pic.m_index;
+            take_pic.register_to_notification(after_take_photo);
+            take_pic.execute();
+        }
+
+
     }
 }
