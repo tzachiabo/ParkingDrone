3241aa06c8c88e78e6619e7986dbd767aca2a1c4
tzachi46 <tzachiabo@gmail.com>
2019-04-12 12:31:54 +0300

    add report manager

---
+++ c#/DroneServer/BL/ReportManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServer.BL
+{
+    class ReportManager
+    {
+        private static ReportManager instance = null;
+
+        private ReportManager()
+        {
+        }
+
+        public static ReportManager getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ReportManager();
+            }
+
+            return instance;
+        }
+
+        public void addCarPlate(string car_plate, string path_to_car_image)
+        {
+
+        }
+
+        public void make_report(string path)
+        {
+
+        }
+    }
+}
